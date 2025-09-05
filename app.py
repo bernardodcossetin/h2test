e964ff33592fbb575e9679b4746131845bf1df68
bernardodcossetin <bernardodcossetin@gmail.com>
2025-09-05 14:40:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,18 @@
 
 import streamlit as st
 
-home_page = st.Page("Home.py",title = "TCO and GHG test")
-page_1 = st.Page("ICEV_page_test.py", title = "ICEV - TCO and GHG")
-page_2 = st.Page("BEV_page_test.py", title = "BEV - TCO and GHG")
-page_3 = st.Page("FCEV_page_test.py", title = "FCEV - TCO and GHG")
-page_4 = st.Page("Comparison.py", title = "Comparison")
-
+pages={
+    "Home Page": [st.Page("Home.py", title = "Home Page"),],
+    "Modes": [
+        st.Page("ICEV_page_test.py", title = "ICEV - TCO and GHG"),
+        st.Page("BEV_page_test.py", title = "BEV - TCO and GHG"),
+        st.Page("FCEV_page_test.py", title = "FCEV - TCO and GHG"),
+        st.Page("Comparison.py", title = "Comparison"),
+    ]  
+}
 pg = st.navigation([home_page,page_1,page_2,page_3,page_4])
 
 pg.run()
+
 
 # iniciação: prompt => streamlit run app.py
