18c3ccbb477479ea6c0d4fdd12cc65e2e29580b1
bernardodcossetin <bernardodcossetin@gmail.com>
2025-09-25 17:47:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown(
             content: "";
             display: block;
             height: 100px;  
-            margin: -10px auto 30px auto;
+            margin: 0px auto 30px auto;
             background-image: url("data:image/png;base64,{img_base64}");
             background-size: contain;
             background-repeat: no-repeat;
@@ -45,4 +45,5 @@ pg = st.navigation(pages)
 
 
 pg.run()
+
 
