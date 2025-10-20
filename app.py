ccd52931e6e97f6b275228af70f620f865d9f990
bernardodcossetin <bernardodcossetin@gmail.com>
2025-10-20 16:22:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,7 @@ def get_base64_image(image_path):
 img_base64 = get_base64_image("LEPTEN.png")
 
 st.set_page_config(layout='wide')
-st.markdown("""
-    <style>
-        
-        [data-testid="stToolbar"] button[kind="icon"] {
-            display: none !important;
-        }
-        
-        
-    </style>
-""", unsafe_allow_html=True)
+
 st.markdown(
     f"""
     <style>
