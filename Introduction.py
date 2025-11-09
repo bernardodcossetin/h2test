c213e8f15fd4e62fb37c0bdda6e550356b9165bb
bernardodcossetin <bernardodcossetin@gmail.com>
2025-11-09 16:18:21 -0300

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -4,6 +4,11 @@ st.title("Introduction")
 
 st.subheader("About the project")
 
+st.markdown(''' The TCO and GHG Simulator is a free and open-source computational model developed by LEPTEN (Laboratory of Process Engineering and Energy Conversion Technologies) starting in 20XX, sponsored by CNPq. Its objective is to perform and present quantitative analyses of the Brazilian automotive market, focusing on greenhouse gas emissions throughout a vehicle’s lifetime, as well as the economic impact for users.
+The project was divided into X stages, and this platform was developed to present the results obtained in the research. The website features four main sections: ICEV, BEV, and FCEV TCO and GHG Analysis, and the Comparison page. The first three sections provide detailed individual results, which can also be compared on the comparison page.  
+
+The model was built in Python based on previously developed models from associated projects. It includes a wide range of input data (vehicle data and Brazilian regional economic data), which will be updated over time. Users can also view and download the original base models as well as the models specific to this project from our repository.
+''')
 
 
 
