d1378d29bdd447228a530dd012435e64eaae6ad9
bernardodcossetin <bernardodcossetin@gmail.com>
2025-11-25 14:04:40 -0300

    Update ModelOverview.py

--- ModelOverview.py
+++ ModelOverview.py
@@ -5,7 +5,6 @@ st.title("Model Overview")
 st.markdown(''' This section provides a general explanation of the computational model used, aiming to clarify the notations, data sources, input fields, where the data comes from, and how the model operates.  
             It is important to note that this explanation is based on a generic version of the model for illustrative purposes. For deeper understanding and complete technical details, we invite users to visit our repository.  
             Contrary to how information is displayed on the website, the following diagram illustrates how the input data is requested from the user, showing which parameters are needed to operate the tool. 
-              
             **The abbreviations stand for:** 
                   
              • al = IPVA tax rate   
@@ -87,6 +86,7 @@ with col2:
     
         return [np.array(GHG),np.array(GHG_km),ghg_accumulated]'''
     st.code(code, language="python")
+
 
 
 
